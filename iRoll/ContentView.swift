e0502f03ff339ef8c9873e382c13256f9c174c9b
Carlossiii <cv5673@gmail.com>
2022-11-15 00:39:39 -0300

    Added counter.

--- iRoll/ContentView.swift
+++ iRoll/ContentView.swift
@@ -9,14 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     
-    let dice6 = [1, 2, 3, 4, 5, 6]
+    let dices = [4, 6, 8, 10, 12, 20, 50, 100]
+    //@State private var timesButton = Int()
+    @State private var selectedDice = Int()
+    @State private var numberOfDice = Int()
     @State private var results = [Int]()
+    @State private var totalRolled = 0
     
     var body: some View {
         NavigationStack {
-            VStack {
-                Button("Roll the dice!") {
-                    results.append(randomDiceNumber(dice: dice6))
+            Form {
+                //VStack {
+                Picker("How many dices?", selection: $numberOfDice) {
+                    ForEach((1..<6), id: \.self) {
+                        Text("\($0)")
+                    }
+                }
+                Picker("Choose a dice!", selection: $selectedDice) {
+                    ForEach(dices, id: \.self) {
+                        Text("\($0)")
+                    }
+                }
+                Button {
+                    totalRolled = 0
+                    randomDiceNumber(howMany: numberOfDice, dice: generateDice(diceFaces: selectedDice))
+                    //timesButton += 1
+                } label: {
+                    Image(systemName: "dice")
                 }
                 Section {
                     List {
@@ -27,16 +46,24 @@ struct ContentView: View {
                 }
             }
             .navigationTitle("iRoll")
+            .toolbar() {
+                Text("Total rolled this time: \(totalRolled)")
+            }
         }
     }
     
-    func randomDiceNumber (dice: [Int]) -> Int {
-        dice.randomElement()!
+    func randomDiceNumber (howMany: Int, dice: [Int]) {
+        var result = Int()
+        for _ in 0..<howMany {
+            result = dice.randomElement()!
+            results.append(result)
+            totalRolled += result
+        }
     }
     
-    func generateDice (dice: Int) -> [Int] {
+    func generateDice (diceFaces: Int) -> [Int] {
         var diceArray = [Int]()
-        for t in 0..<dice {
+        for t in 0..<diceFaces {
             diceArray.append(t)
         }
         return diceArray
