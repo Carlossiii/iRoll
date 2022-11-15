f8b567775648cdbb6622206bcc7c949e1ba1b57d
Carlossiii <cv5673@gmail.com>
2022-11-14 21:48:32 -0300

    Initial Commit

---
+++ iRoll/iRollApp.swift
@@ -0,0 +1,17 @@
+//
+//  iRollApp.swift
+//  iRoll
+//
+//  Created by Carlos Vinicius on 14/11/22.
+//
+
+import SwiftUI
+
+@main
+struct iRollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
